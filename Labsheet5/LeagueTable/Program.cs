b0b6659c430c4c0af53c24b62af55927310462e1
Janessa Aulén <S00190968@mail.itsligo.ie>
2018-11-06 15:59:21 +0000

    things

---
+++ Labsheet5/LeagueTable/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Team[] teams = new Team[5];
+
+            Team SligoRovers = new Team("Sligo  Rovers");
+            Team FinnHarps = new Team("Finn  Harps");
+            Team GalwayUnited = new Team("Galway  United");
+            Team DerryCity = new Team("Derry  City");
+            Team Dundalk = new Team("Dundalk");
+
+            teams.Add(SligoRovers);
+            teams.Add(FinnHarps);  
+            teams.Add(GalwayUnited);
+            teams.Add(DerryCity);
+            teams.Add(Dundalk);
+
+            printTeamsdetails(teams);
+
+            SligoRovers.AddResult(Team.Result.Win);
+            FinnHarps.AddResult(Team.Result.Lose);
+            FinnHarps.AddResult(Team.Result.Draw);
+            GalwayUnited.AddResult(Team.Result.Win);
+            Dundalk.AddResult(Team.Result.Lose);
+
+            printTeamsdetails(teams);
+
+            //List<Team>.Sort(teams);
+
+        }
+
+        public static void printTeamsdetails(List<Team> teams)
+        {
+            Console.WriteLine(string.Format("{0,20} {1,20} {2,20} {3, 20} {4, 20}", "Team name:", " Points:", "Wins:", "Losses:", "Draws:"));
+            Console.WriteLine();
+            foreach (Team team in teams)
+            {
+                team.printTeamDetails();
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ Labsheet5/LeagueTable/Team.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueTable
+{
+    class Team : IComparable
+    {
+
+        private string name;
+        private string nmanagerNme;
+        private int points;
+        private int wins;
+        private int losses;
+        private int draws;
+        private int games;
+
+        public Team(string name) : this(name, "Not added")
+        {
+            this.name = name;
+            this.points = 0;
+            this.wins = 0;
+            this.losses = 0;
+            this.draws = 0;
+            this.games = 0;
+        }
+
+        public Team(string teamname, string managername)
+        {
+            Name = teamname;
+            ManagerName = managername;
+        }
+
+        public string Name { get; private set; }
+        public string ManagerName { get; private set; }
+        public int Points { get; private set; }
+        public int Wins { get; private set; }
+        public int Losses { get; private set; }
+        public int Draws { get; private set; }
+        public int Games { get; private set; }
+
+        public enum Result { Win, Draw, Lose};
+
+        public void AddResult(Result currentResult)
+        {
+            switch (currentResult)
+            {
+                case Result.Win:
+                    Wins ++;
+                    Points += 3;
+                    Games ++;
+                    break;
+                case Result.Lose:
+                    Games++;
+                    Losses++;
+                    break;
+                case Result.Draw:
+                    Games++;
+                    Draws++;
+                    break;
+            }
+        }
+
+        public void printTeamDetails()
+        {
+            Console.WriteLine(string.Format("{0,20} {1,20} {2,20} {3, 20} {4, 20}", name, points, wins, losses, draws));
+        }
+
+        int IComparable.CompareTo(object obj)
+        {
+            //throw new NotImplementedException();
+            Team that = (Team)obj;
+            return that.Points.CompareTo(this.Points);
+        }
+    }
+}
